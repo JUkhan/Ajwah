7ad9f5a4d4dfda5fe53f7caf49bf26e9fe90767a
jukhan <jukhan@Kamruls-MacBook-Pro.local>
2019-05-08 11:45:38 +0600

    react-ajwah

--- modules/react-ajwah/index.d.ts
+++ modules/react-ajwah/index.d.ts
@@ -1,3 +1,4 @@
 
 export declare function Connect(splitedState?: { [key: string]: (state: any) => any }, componentInstance?: any): any;
+export declare function useSubscriptions<S>(states: string[]): S;
 export declare function useSubscriptions(states: string[]): { [key: string]: any };
